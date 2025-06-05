3ea0c9095142e378b175f7ab39e715c1bac6db28
eswar21-sudo <eswarkumar5475@gmail.com>
2025-06-05 13:23:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "spacelift-vpc" {
   version = "0.1.0"
 
   cidr_block           = "10.0.0.0/16"
+  region = "us-east-1"
   enable_dns_support   = true
   enable_dns_hostnames = true
   vpc_name             = "demo"
