b695704aaf1f871063d5cf92b4fdc7058144f07e
eswar21-sudo <eswarkumar5475@gmail.com>
2025-06-05 10:46:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   version = "0.1.0"
 
   cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   vpc_name             = "demo"
 }
